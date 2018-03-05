f9f9c476c2f1280cf00b3c69fa08bd60d415166f
Aleks.Creative <aleksdesign@yahoo.co.uk>
2018-03-05 20:41:20 +0000

    update Pixel maker from challenge part - copy from my codePen

--- 01_FEND_Challenge-2017/pixel maker/project-pixel-art-maker-starter-master/designs.js
+++ 01_FEND_Challenge-2017/pixel maker/project-pixel-art-maker-starter-master/designs.js
@@ -1,38 +1,48 @@
-/*----Declare variables----*/
 /* Grid body */
 const tableBody = $("#pixel-canvas");
 
 /* Grid submit button */
 const gridSubmit = $("#button-submit");
 
-/* Select size of the grid */
-var selectHeight = $("#input-height").val();
-var selectWidth = $("#input-width").val();
+/* ---- Making Grid ----- */
+function makeGrid() {
+  /* Select size of the grid */
+let selectHeight = $("#input-height").val();
+let selectWidth = $("#input-width").val();
+  /* Clears the table initially and prevents making more tables on further clicks */
+  tableBody.empty();
 
-/* Colour picker */
-var colourPick = $("#color-picker")
+  for (let i = 0; i < selectHeight; i++) {
+    tableBody.append("<tr></tr>");
 
-/* Submit grid size */
-$("#button-submit").on("click", function(){
+    for (let j = 0; j < selectWidth; j++) {
+      tableBody.find("tr").last().append("<td></td>");
+    }
+  }
+}
+
+/* Submit grid size on click */
+gridSubmit.click(function(event){
+  event.preventDefault();
   makeGrid();
 });
 
-/*--------Function make grid-------*/
-function makeGrid() {
-/* clear table */
-tableBody.empty();
-/* Draw rows of the table */
-for(var i=0; i < selectHeight; i++){
-  tableBody.append('<tr></tr>');
-  /* Draw cells */
-  for(var j=0; j < selectWidth; j++){
-    tableBody.find("tr").last().append ("<td></td>");
+/*-----Draw function -----*/
+
+  /* Pick colour */
+function drawPixels(){
+  tableBody.on("click dragover", "td", function(){
+    let colourPick = $("#color-picker").val();
+    $(this).css("background-color", colourPick);
+    });
   }
-}};
+drawPixels();
 
-/*-----Colour picker function -----*/
-
-tableBody.click(function(event){
-  var colour = colourPick.val();
-  $(event.target).css("background-color", colour);
-});
+  /*Clear canvas */
+function clearCanvas(){
+  let clearButton = $("#button-clear");
+  clearButton.on("click", function(){
+    $("td").css("background-color", "transparent");
+  });
+}
+clearCanvas();
