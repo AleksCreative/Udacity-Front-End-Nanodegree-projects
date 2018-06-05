765eaf230817f3f54c6de18d7421e89439ab4958
AleksCreative <aleksdesign@yahoo.co.uk>
2018-06-05 21:47:21 +0100

    Place the player object in a variable called player

--- 02_FEND_Nanodegree-2018/Project04 - Arcade Game clone/js/app.js
+++ 02_FEND_Nanodegree-2018/Project04 - Arcade Game clone/js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -22,15 +22,18 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+var Player = function(x, y){
+
+this.sprite = 'images/char-boy.png';
 // This class requires an update(), render() and
 // a handleInput() method.
-
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 // Place the player object in a variable called player
-
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
