b75d3468fa9a0e52015381f04f6d15be412b4744
AleksCreative <aleksdesign@yahoo.co.uk>
2018-07-07 23:38:12 +0100

    add new test suite named the menu and test if menu is hidden by default

--- 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
+++ 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
@@ -35,8 +35,8 @@ $(function() {
            allFeeds.forEach(function(feed){
              expect(feed.url).toBeDefined();
              expect(feed.url).not.toBe(0);
-           })
-         })
+           });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
@@ -46,25 +46,31 @@ $(function() {
            allFeeds.forEach(function(feed){
              expect(feed.name).toBeDefined();
              expect(feed.name).not.toBe(0);
-           })
-         })
+           });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('is hidden', function(){
+           const body = $('body');
+           expect(body).hasClass('menu-hidden').toBe('true');
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
