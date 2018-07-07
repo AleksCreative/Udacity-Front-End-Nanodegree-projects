7242ecf88f8a6ca03062c546cbc439fc3e2a2278
AleksCreative <aleksdesign@yahoo.co.uk>
2018-07-07 23:20:18 +0100

    add test if feed name is defined and if is not empty

--- 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
+++ 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
@@ -42,7 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        
+         it('has name defined and is not empty', function() {
+           allFeeds.forEach(function(feed){
+             expect(feed.name).toBeDefined();
+             expect(feed.name).not.toBe(0);
+           })
+         })
     });
 
 
