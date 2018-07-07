b26a30139bd2e90f6e2daa1c852b746657b2482c
AleksCreative <aleksdesign@yahoo.co.uk>
2018-07-08 00:28:53 +0100

    add new test suite Initial Entries

--- 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
+++ 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
@@ -79,8 +79,10 @@ $(function() {
 
 
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
-
+    /* Test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+      
+    })
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
