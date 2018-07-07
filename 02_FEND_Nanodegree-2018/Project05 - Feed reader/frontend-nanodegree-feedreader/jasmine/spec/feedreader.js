81cb1d4f74cc0e2e4d727c2b86d0b43552dde5bc
AleksCreative <aleksdesign@yahoo.co.uk>
2018-07-07 23:19:30 +0100

    add test if feed url is defined and not empty

--- 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
+++ 02_FEND_Nanodegree-2018/Project05 - Feed reader/frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
@@ -31,12 +31,18 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('has URL defined and is not empty', function() {
+           allFeeds.forEach(function(feed){
+             expect(feed.url).toBeDefined();
+             expect(feed.url).not.toBe(0);
+           })
+         })
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        
     });
 
 
