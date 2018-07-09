5882bca78b4a7d90ef3326c080df748b5ce0c1b5
AleksCreative <aleksdesign@yahoo.co.uk>
2018-07-09 19:42:09 +0100

    cat clicker with 5 cat's specification(not working)

--- 02_FEND_Nanodegree-2018/exercises/core4_cat-clicker/js/app.js
+++ 02_FEND_Nanodegree-2018/exercises/core4_cat-clicker/js/app.js
@@ -1,10 +1,14 @@
-const catPicture = document.getElementsByClassName('cat-picture');
-const clickCounter = document.getElementsByClassName('click-count');
-const catName = document.getElementsByClassName('cat-name')
+const catPicture = document.getElementById('cat-picture');
+const clickCounter = document.getElementById('click-count');
+const catName = document.getElementsByClassName('cat-name');
+const catList = document.getElementById('cat-list');
 
-function Cat(name, clicks) {
+function Cat(name, clicks, picture) {
   this.name = name;
   this.clicks = clicks;
+  this.picture = function(picture) {
+    catPicture.innerHTML = '<img src="img/"' + picture + '>'
+  };
   this.changeOpacity = function() {
     this.classList.toggle('change-opacity');
     setTimeout(function() {
@@ -12,53 +16,54 @@ function Cat(name, clicks) {
     }, 500);
   }
   this.updateClicks = function() {
-    this.clicks++;
-  }
-}
-
-let cat01 = new Cat('Kiki', 0);
-let cat02 = new Cat('Slinki', 0);
-
-catName[0].textContent = cat01.name;
-catName[1].textContent = cat02.name;
-
-/**function updateClicks() {
-  catPicture[0].classList.toggle('change-opacity');
-  catPicture[1].classList.toggle('change-opacity');
-  setTimeout(function() {
-    catPicture[0].classList.toggle('change-opacity');
-    catPicture[1].classList.toggle('change-opacity');
-  }, 500);
-
-  for (let m = 0; m < cat01.clicks; m++) {
-    if (cat01.clicks === 1) {
-      clickCounter[0].textContent = cat01.clicks + ' time';
-    } else {
-      clickCounter[0].textContent = cat01.clicks + ' times';
+    for (let m = 0; m < this.clicks; m++) {
+      if (cat01.clicks === 1) {
+        clickCounter[0].textContent = this.clicks + ' time';
+      } else {
+        clickCounter[0].textContent = this.clicks + ' times';
+      }
     }
   }
-} */
+  this.catClick = function() {
+    catPicture.addEventListener('click', updateClicks);
+  }
+}
 
 
+let cat01 = new Cat('Kiki', 0, 'cat.jpg');
+let cat02 = new Cat('Pussy', 0);
+let cat03 = new Cat('Blacky', 0);
+let cat04 = new Cat('Boo', 0);
+let cat05 = new Cat('Slinki', 0);
+
+let catArray = [cat01, cat02, cat03, cat04, cat05];
+let listItem = ' ';
+let catSingle = catList.children;
+
+
+function makeList() {
+  for (let i = 0; i < catArray.length; i++) {
+  listItem = document.createElement('li');
+  catList.appendChild(listItem);
+  }
+
+  let listItems = document.querySelectorAll('li')
+  for (let j = 0; j < catArray.length; j++) {
+    listItems[j].textContent = 'This is ' + catArray[j].name;
+  }
+}
+makeList();
 
 function clickCat() {
-  catPicture[0].addEventListener('click', cat01.updateClicks);
-}
-function clickCat2() {
-  catPicture[1].addEventListener('click', cat02.updateClicks);
-}
-
-if (cat01.clicks === 1) {
-  clickCounter[0].textContent = cat01.clicks + ' time';
-} else {
-  clickCounter[0].textContent = cat01.clicks + ' times';
+  catList.addEventListener('click', function(e) {
+    catSingle = e.target;
+    generateCat();
+  });
 }
 
-if (cat02.clicks === 1) {
-  clickCounter[1].textContent = cat01.clicks + ' time';
-} else {
-  clickCounter[1].textContent = cat01.clicks + ' times';
+function generateCat() {
+  for (let k = 0; k < catArray.length; k++) {
+    catName.textContent = 'My name is ' + catArray[k].name;
+  }
 }
-
 clickCat();
-clickCat2();
